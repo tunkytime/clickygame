2f2656d7385d83797401972e73bace62fbc3297b
aschaaf <schaaf1091@gmail.com>
2019-06-22 16:18:31 -0600

    Updates

---
+++ precache-manifest.b92189fc594ca9ac030a73f9778e4402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7a5e52800dd6892123138170355ab9",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "f452893056d134b587a5",
+    "url": "/clickygame/static/css/main.fd553abc.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f452893056d134b587a5",
+    "url": "/clickygame/static/js/main.7d7a7fcb.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
